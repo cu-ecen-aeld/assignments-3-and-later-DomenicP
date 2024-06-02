fab93442720597285f49d2ca6a70ef0a200aaafb
Domenic Rodriguez <domenicpaul@me.com>
2024-06-02 22:34:46 +0000

    Setup environment and fix validate username test

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Development environment flake for ECEA 5305";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cmake
+          ruby
+        ];
+      };
+    };
+}
